b7e66712dd78de68c00d6c0a1bdd79e4604cf734
Piotr Ceberek <piotr.ceberek@wp.pl>
2026-05-16 18:02:46 +0200

    Added slider

--- client.py
+++ client.py
@@ -25,10 +25,10 @@ def receive_messanges(client_socket,canvas,my_color):
                         my_color[0] = line.split(",")[1]
                     elif line:
                         coords = line.split(',')
-                        if len(coords) == 6:
+                        if len(coords) == 7:
                             try:
-                                x1, y1, x2, y2 = map(int, coords[2:])
-                                canvas.create_line(x1, y1, x2, y2, fill=coords[0], width=2)
+                                x1, y1, x2, y2 = map(int, coords[3:])
+                                canvas.create_line(x1, y1, x2, y2, fill=coords[0], width=int(coords[2]),capstyle="round")
                             except ValueError:
                                 continue
 
@@ -41,12 +41,13 @@ def save_position(event,mouse_pos,stroke):
     mouse_pos['y'] = event.y
     stroke[0] += 1
 
-def draw_and_send(event,mouse_pos,canvas,my_socket,stroke,my_color):
+def draw_and_send(event,mouse_pos,canvas,my_socket,stroke,my_color,gauge):
+    actual_number = gauge.get()
     last_x = mouse_pos['x']
     last_y = mouse_pos['y']
     if last_x is not None and last_y is not None:
-        canvas.create_line(last_x,last_y,event.x,event.y,fill = my_color[0], width = 2)
-        msg = f"{stroke[0]},{last_x},{last_y},{event.x},{event.y}\n"
+        canvas.create_line(last_x,last_y,event.x,event.y,fill = my_color[0], width = actual_number,capstyle="round")
+        msg = f"{stroke[0]},{actual_number},{last_x},{last_y},{event.x},{event.y}\n"
         my_socket.sendall(msg.encode('utf-8'))
     mouse_pos['x'] = event.x
     mouse_pos['y'] = event.y
@@ -59,17 +60,20 @@ def undo_previous(my_socket):
     my_socket.sendall("UNDO\n".encode('utf-8'))
 
 def client_main():
+    root = tkinter.Tk()
     mouse_pos = {'x': None, 'y': None}
     my_color = ["black"]
     stroke = [0]
     my_socket = setup()
-    root = tkinter.Tk()
+    gauge = tkinter.IntVar(value = 2)
     canvas = tkinter.Canvas(root,width=800,height=600,bg = 'white')
+    slider = tkinter.Scale(root,from_ = 1, to = 20, orient='horizontal',resolution = 1, variable = gauge)
     canvas.pack()
+    slider.pack()
     canvas.focus_set()
     threading.Thread(target=receive_messanges, args=(my_socket, canvas,my_color), daemon=True).start()
     canvas.bind("<Button-1>",lambda e: save_position(e,mouse_pos,stroke))
-    canvas.bind("<B1-Motion>",lambda e: draw_and_send(e,mouse_pos,canvas,my_socket,stroke,my_color))
+    canvas.bind("<B1-Motion>",lambda e: draw_and_send(e,mouse_pos,canvas,my_socket,stroke,my_color,gauge))
     canvas.bind("<c>",lambda e: clear_all(my_socket,canvas))
     canvas.bind("<z>",lambda e: undo_previous(my_socket))
     root.mainloop()
